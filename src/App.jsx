2d90407623b0f1c439a80a3915924d5f7b830115
MagedGamail <magedgamail2391@gmail.com>
2025-12-09 14:53:53 +0200

    Fixing Api Cart-items

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,10 @@ window.axios = axios;
 
 function App() {
   const [cart, setCart] = useState([]);
+  const backendLink = 'https://ecommerce-backend-production-c5c1.up.railway.app';
 
   const loadCart = async () => {
-      const response = await axios.get("/api/cart-items?expand=product")
+      const response = await axios.get(`${backendLink}/api/cart-items?expand=product`)
         setCart(response.data);
     };
 
