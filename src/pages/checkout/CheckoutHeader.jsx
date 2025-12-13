c8765c2cc671dac133cc03a3f063e7fecc7d0798
MagedGamail <magedgamail2391@gmail.com>
2025-12-13 15:14:45 +0200

    Developing

--- src/pages/checkout/CheckoutHeader.jsx
+++ src/pages/checkout/CheckoutHeader.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router';
 import CheckoutIcon from '../../assets/images/icons/checkout-lock-icon.png'
-import logo from '../../assets/images/logo.png'
+//import logo from '../../assets/images/logo.png'
 import MobileLogo from '../../assets/images/mobile-logo.png'
 
 import './checkoutHeader.css';
