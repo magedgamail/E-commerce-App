f63b770bd71fd4f1555cb2dab0539b15a3db5f9b
MagedGamail <magedgamail2391@gmail.com>
2025-12-13 13:37:13 +0200

    Developing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -49,11 +49,12 @@ export function Header( { cart=[] } ) {
             <div className="header">
                 <div className="left-section">
                     <NavLink to="/" className="header-link">
-                    <div className='logo-1'><div className='m'>M</div>aged</div>
-                        {/*<img className="logo"
-                            src={ LogoWhite } />*/}
-                        {/* <img className="mobile-logo"
-                            src={ MobileLogoWhite } /> */}
+                    {/* <div className='logo-1'><div className='m'>M</div>aged</div> */}
+                    
+                        <img className="logo"
+                            src={ LogoWhite } />
+                        <img className="mobile-logo"
+                            src={ MobileLogoWhite } />
                     </NavLink>
                 </div>
 
