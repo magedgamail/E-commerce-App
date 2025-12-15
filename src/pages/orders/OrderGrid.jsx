f610d2c47a03847d14d1585ab30b891f20f3142c
MagedGamail <magedgamail2391@gmail.com>
2025-12-15 14:43:01 +0200

    Developing

--- src/pages/orders/OrderGrid.jsx
+++ src/pages/orders/OrderGrid.jsx
@@ -53,7 +53,7 @@ export function OrderGrid({ orders, loadCart }) {
                                         </div>
 
                                         <div className="product-actions">
-                                            <a href={`${backendMainLink}/${order.id}/${orderProduct.product.id}`}>
+                                            <a href={`/tracking/${order.id}/${orderProduct.product.id}`}>
                                                 <button className="track-package-button button-secondary">
                                                     Track package
                                                 </button>
